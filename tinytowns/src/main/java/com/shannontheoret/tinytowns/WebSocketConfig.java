fad4a86db45ca23692fb2fcfd5f5617c30c13a44
Shannon Theoret <shannon.theoret@outlook.com>
2025-06-28 14:24:20 -0400

    Include protocol for ws config

--- tinytowns/src/main/java/com/shannontheoret/tinytowns/WebSocketConfig.java
+++ tinytowns/src/main/java/com/shannontheoret/tinytowns/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173", "tinytowns.shannontheoret.com")
+                .setAllowedOrigins("http://localhost:5173", "http://tinytowns.shannontheoret.com")
                 .withSockJS();
     }
 
