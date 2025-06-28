94c09922bb8c0b0761a737eb2ad5db01a7c5daaf
Shannon Theoret <shannon.theoret@outlook.com>
2025-06-28 10:12:48 -0400

    Change api file

--- tiny-towns/src/api.js
+++ tiny-towns/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const instance = axios.create({
-  baseURL: {API_BASE_URL},
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
